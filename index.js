27f3e7538cabfa2e546469413806df27542acaf9
ali husnain <you@example.com>
2023-07-08 17:00:07 +0500

    synchronous vs asynchronous & Bolocking vs Non-bolocking in fs

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // import module
-
 const fs = require('fs');
 
+// Bolocking synchronous way
+/*
 const textIn = fs.readFileSync('./txt/input.txt','utf-8',);
 console.log(textIn);
-
 const textOut = `This what is we know about the avocados: ${textIn}.\nCreate on ${Date.now()}`;
-
 fs.writeFileSync('./txt/output.txt',textOut);
-
 console.log(`Written File => ${textOut}`);
+*/
 
+// Non-bolocking asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8' ,(err,data1) => {
+    if(err) {
+        return console.log("Error ! 🎇");
+    }
+    console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err,data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt' , 'utf-8' , (err,data3) => {
+            console.log(data3);
+            fs.writeFile('/.txt/final.txt',`${data2}\n${data3}` , (err) => {
+                console.log('your file is written');
+            })
+            
+        })
+    })
+
+})
