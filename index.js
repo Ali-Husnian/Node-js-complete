2aec08f4c912cd55c2ebdc0503ef698da34b79dd
ali husnain <you@example.com>
2023-07-15 16:18:02 +0500

    Basic api fetch data from data.json file and routing

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ fs.readFile('./txt/start.txt', 'utf-8' ,(err,data1) => {
 /**************************/
 /*         SERVER         */
 /**************************/
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req,res) =>{
     const pathName = req.url;
     
@@ -44,7 +47,11 @@ const server = http.createServer((req,res) =>{
         res.end('This is the OVWRVIEW');
     } else if(pathName === '/product'){
         res.end('This is the PRODUCT');
-    } else{  // 404 page
+    } else if(pathName === '/api'){
+        res.writeHead(200,{'Content-type':'application/json'})
+        res.end(data);
+    }   
+    else{  // 404 page
         res.writeHead(404,{
             'Content-type':'text/html',
             'my-own-header':'hello world'
