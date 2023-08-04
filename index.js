cbb2523050d6492cf99c0a17069a9b657fe0493a
ali husnain <you@example.com>
2023-08-04 16:27:10 +0500

    promises .then or .catch Method

--- index.js
+++ index.js
@@ -3,6 +3,23 @@
 const fs = require("fs");
 const superagent = require('superagent');
 
+// fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
+//     if(err) return console.log(err.message);
+
+//     console.log(`Breed: ${data}`);
+//     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err,res) => {
+//         console.log(res.body.message);
+
+//         fs.writeFile('dog-img.txt',res.body.message , (err) => {
+//             if(err) return console.log(err.message);
+//             console.log('Rendom Dog image saved in file');
+//         })
+//     })
+// })
+
+///////////////////////////////////////////////////
+///////// promises .then or .catch Method
+
 fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
     
     console.log(`Breed: ${data}`);
