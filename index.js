141db8011f07a5a4f95768a3296ad1051061e0b0
ali husnain <you@example.com>
2023-08-04 16:00:10 +0500

    Async node.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// https://dog.ceo/dog-api/documentation/breed ===> https://dog.ceo/api/breed/rottweiler/images/random
+
+const fs = require("fs");
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
+    
+    console.log(`Breed: ${data}`);
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then(res => {
+        console.log(res.body.message);
+
+        fs.writeFile('dog-img.txt',res.body.message , (err) => {
+            if(err) return console.log(err.message);
+            console.log('Rendom Dog image saved in file');
+        })
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+})
