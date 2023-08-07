f8a46b6b131ac83da556e72327ff1daa95e849f1
ali husnain <you@example.com>
2023-08-07 13:21:43 +0500

    Async Await and try catch method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const superagent = require('superagent');
 
+
 // fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
 //     if(err) return console.log(err.message);
 
@@ -20,12 +21,14 @@ const superagent = require('superagent');
 ///////////////////////////////////////////////////
 ///////// promises .then or .catch Method
 
+/*
 fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
-    
+    // without promise
     console.log(`Breed: ${data}`);
 
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res => {
+    .then(res => { // resolved promises
+        // full fill success
         console.log(res.body.message);
 
         fs.writeFile('dog-img.txt',res.body.message , (err) => {
@@ -33,7 +36,64 @@ fs.readFile(`${__dirname}/dog.txt`,(err,data) => {
             console.log('Rendom Dog image saved in file');
         })
     })
-    .catch(err => {
+    .catch(err => {  // reject promis
+        // throw error
         console.log(err.message);
     })
 })
+
+
+*/
+/////////// final
+const readFilePromis = (file) => {
+    return new Promise( (resolve,reject) => {
+        fs.readFile(`${file}`, (err,data) => {
+            if(err) reject('Could not find that file 🤣😂😂');
+            resolve(data);
+        })
+    })
+}
+const writeFilePromise = ((file,data) => {
+    return new Promise( (resolve,reject) => {
+        fs.writeFile(file,data, (err) => {
+            if(err) reject('Could not write file 🤣😂😂');
+            resolve('success');
+        })
+    })
+})
+
+const getDogPic = async () => {
+    try {
+        const data = await readFilePromis(`${__dirname}/dog.txt`)
+        console.log(`Breed: ${data}`);
+    
+        const res = await superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`);
+        console.log(res.body.message);
+    
+        await writeFilePromise('dog-img.txt',res.body.message);
+        console.log('Rendom Dog image saved in file');
+    } catch (err) {
+        console.log(err);
+    }
+   
+}
+getDogPic();
+
+   /*
+    readFilePromis(`${__dirname}/dog.txt`).then(data => {
+        console.log(`Breed: ${data}`);
+        return superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    })
+    .then( (res) => { 
+        console.log(res.body.message);
+        return writeFilePromise('dog-img.txt',res.body.message);
+    }).then( () => {
+        console.log('Rendom Dog image saved in file');
+    })
+    .catch(err => {  
+        console.log(err);
+    })
+   */
+///////////// final end
